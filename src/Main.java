4c9e53b525940c050d56c5e9024ff6b4add3b4a3
joelmunu <joel.munu@gmail.com>
2023-01-17 22:42:50 +0000

    Terminado el experimento dos

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,9 @@ public class Main {
 
         Huerto huerto = new Huerto(5);
         Granjero granjero1 = new Granjero("Lucas", 10, 5,huerto);
-        Consumidor consumidor1 = new Consumidor("Pedro", 5, 5, huerto);
-        Consumidor consumidor2 = new Consumidor("Luis", 3, 5, huerto);
+        Consumidor consumidor1 = new Consumidor("Pedro", 5, 10, huerto);
+
         granjero1.start();
         consumidor1.start();
-        consumidor2.start();
     }
 }
