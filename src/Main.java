b66f9fb4fcd172af0ee40117534ac4d5671fe937
joelmunu <joel.munu@gmail.com>
2023-01-18 10:26:17 +0000

    Terminado el experimento-tres

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
 
         Huerto huerto = new Huerto(5);
-        Granjero granjero1 = new Granjero("Lucas", 10, 5,huerto);
+        Granjero granjero1 = new Granjero("Lucas", 5, 5,huerto);
         Consumidor consumidor1 = new Consumidor("Pedro", 5, 5, huerto);
-        Consumidor consumidor2 = new Consumidor("Luis", 3, 5, huerto);
+
+        granjero1.setPriority(10);
         granjero1.start();
         consumidor1.start();
-        consumidor2.start();
     }
 }
